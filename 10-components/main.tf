0f0e4b5f74fb33ace8a9f4b9d1804ef4fcdb6353
GautamKumar <kodukulagautamkumar@gmail.com>
2025-07-07 19:26:08 +0530

    new change

--- 10-components/main.tf
+++ 10-components/main.tf
@@ -1,5 +1,6 @@
-module "user" {
-    source = "../../terraform-aws-roboshop"
-    component = "user"
-    rule_priority = 20
+module "component" {
+    for_each = var.component
+    source = "git::https://github.com/KodukG01/terraform-aws-roboshop.git?ref=main"
+    component = each.key
+    rule_priority = each.key.rule_priority
 }
